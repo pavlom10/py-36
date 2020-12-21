9327c5f6b6bc5fed54defdbde40150fadbfcb43d
pavlom <10door@gmail.com>
2020-12-21 19:47:04 +0300

    Task 2

--- oop_1/quiz.py
+++ oop_1/quiz.py
@@ -8,7 +8,14 @@ class Student:
         self.grades = {}
 
     def add_courses(self, course_name):
-        self.finished_course.append(course_name)
+        self.finished_courses.append(course_name)
+
+    def rate_lecturer(self, lecturer, grade):
+        if isinstance(lecturer, Lecturer) and len(set(self.courses_in_progress).intersection(lecturer.courses_attached)) > 0:
+            lecturer.grades.append(grade)
+        else:
+            # print('Нет общих курсов')
+            return 'Ошибка'
 
 
 class Mentor:
@@ -17,6 +24,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = []
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -26,14 +41,15 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
 
 cool_lecturer = Lecturer('Some', 'Buddy')
 cool_reviewer = Reviewer('Another', 'Man')
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+
+best_student.courses_in_progress += ['Python']
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate_lecturer(cool_lecturer, 10)
 
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.courses_in_progress += ['Python']
